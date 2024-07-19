57566395070ce5c1be5fe2e8df823f75a874a42c
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-07-19 16:15:11 -0600

    commit whatever I want

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,16 +1,25 @@
-import {GoABAccordion, GoABButton, GoABBadge} from '@abgov/angular-components';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import {GoABAccordion, GoABButton, GoABBadge, GoABAccordionHeadingContent} from '@abgov/angular-components';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from "@angular/core";
+import {AccordionModule} from "primeng/accordion";
+import {CardModule} from "primeng/card";
+import {MatFormField} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [GoABAccordion, GoABButton, GoABBadge],
+  imports: [GoABAccordion, GoABButton, GoABBadge, AccordionModule, CardModule, MatFormField, GoABAccordionHeadingContent],
   templateUrl: './accordion.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AccordionComponent {
+export class AccordionComponent implements OnInit{
   open = false;
+  content = "";
   onClick() {
     this.open = !this.open;
+  }
+  ngOnInit() {
+    setTimeout(() => {
+      this.content = "Content loaded!";
+    }, 2000);
   }
 }
