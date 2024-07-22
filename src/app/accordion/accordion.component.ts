35082dec99de486db139cf00cbad9d9c822f7519
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-07-22 15:44:08 -0600

    update

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -3,11 +3,14 @@ import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from "@angular/core";
 import {AccordionModule} from "primeng/accordion";
 import {CardModule} from "primeng/card";
 import {MatFormField} from "@angular/material/form-field";
+import {MatAccordion} from "@angular/material/expansion";
+import {MatActionList} from "@angular/material/list";
+import {AutoCompleteModule} from "primeng/autocomplete";
 
 @Component({
   selector: 'app-accordion',
   standalone: true,
-  imports: [GoABAccordion, GoABButton, GoABBadge, AccordionModule, CardModule, MatFormField, GoABAccordionHeadingContent],
+  imports: [GoABAccordion, GoABButton, GoABBadge, AccordionModule, CardModule, MatFormField, GoABAccordionHeadingContent, MatAccordion, MatActionList, AutoCompleteModule],
   templateUrl: './accordion.component.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
