9bab58adf69c27a46977b259711d021c696eedae
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-09-20 15:39:14 -0600

    add more

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -9,8 +9,13 @@ import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DatePickerComponent {
+  value = "Fri Apr 26 2024 10:43:28 GMT-0400 (Eastern Daylight Time)";
   onChange(event: Event) {
     // handle change
     console.log((event as CustomEvent).detail.value);
+    this.value = (event as CustomEvent).detail.value;
+  }
+  reset() {
+    this.value = "";
   }
 }
